e395119676cd3aaa75ea1e1ba910d9a02b1b972c
Nadiana Kelly <82840116+Nadiana-Kelly@users.noreply.github.com>
2022-07-26 14:09:04 -0300

    Add files via upload

---
+++ potencia_deslocamento/potencia_inversa.py
@@ -0,0 +1,10 @@
+import numpy as np
+import potencia_regular as pr
+
+#metodo da potencia inversa nada mais é do que o metodo da potencia regular aplicada em cima da matriz inversa
+def Potencia_inversa(matriz, vetor, epson):
+    a1_inversa = np.linalg.inv(matriz)
+    lambda1_a1, x_dominante = pr.Potencia_Regular(a1_inversa, vetor, epson) #vai devolver o autovalor dominante da matriz inversa (lambda)
+    x_n = x_dominante
+    lambda_n_a1 = 1/lambda1_a1 #lambda N é o inverso do lambda dominante da matriz inversa
+    return [lambda_n_a1, x_n] #autovalor e autovetor
---
+++ potencia_deslocamento/potencia_regular.py
@@ -0,0 +1,45 @@
+from math import sqrt
+import numpy as np
+   
+def Potencia_Regular(matriz, vetor, epson): # step 1
+    tam = len(vetor)
+    vk_novo = []
+    vk_velho = []
+    erro = 10
+    lambda1_novo = 0   # step 2
+    for i in range(tam):   # step 3
+        vk_novo.append(vetor[i])
+        vk_velho.append(0)
+        
+    while(erro > epson):
+        lambda1_velho = lambda1_novo   # step 4
+        vk_velho = vk_novo   # step 5
+        x1_velho = normalizacao(vk_velho, tam)   # step 6
+        vk_novo = matriz_x_vetor(x1_velho, matriz)   # step 7
+        lambda1_novo = vetor_x_vetor(x1_velho, vk_novo, tam)   # step 8 aqui multiplicamos o novo vetor pelo vetor old normalizado e obtemos o lambda
+        erro = abs((lambda1_novo-lambda1_velho)/lambda1_novo)   # step 9
+
+    return [lambda1_novo, x1_velho]
+
+def normalizacao(v, tam):
+    aux = []
+    s = sqrt(vetor_x_vetor(v, v, tam)) #aqui tiramos a norma do vetor: <3, 4> = raizQ de 3²+4² = 5
+    for i in range(tam):
+        aux.append(v[i]/s)
+    return aux #<3/5, 4/5>
+
+def matriz_x_vetor(v, matriz):
+    x = []
+    tam = len(matriz)
+  
+    for i in range(tam):
+        s = vetor_x_vetor(matriz[i], v, tam) #faço a multiplicação da matriz pelo vetor e coloco no x
+        x.append(s)
+    return x
+
+def vetor_x_vetor(v1, v2, tam):
+    s = 0
+    for i in range(tam):
+        s = s + v1[i]*v2[i]
+    
+    return s
